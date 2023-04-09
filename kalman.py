19a0947e5d14de9cd79499557db4c63cf7394feb
HesaraMahela <hesaramahela@outlook.com>
2023-04-09 19:51:33 +0530

    basic set up

---
+++ kalman.py
@@ -0,0 +1,22 @@
+STD_DEV_GYRO = 8   # This is the standard deviation of the gyroscope readings. Change this manually to reduce the uncertainity of the measurement.
+STD_DEV_ACC = 2    # This is the standard deviation of the accelerometer readings. Change this manually to reduce the uncertainity of the measurement.
+DELTA_T = 0.01
+
+def kalmanFilter(kalmanAng, kalmanInput, kalmanMeasurement, ):
+    # 1D Kalman filter implementation
+    # kalmanInput = gyro reading, kalmanMeasurement = tilt angle by the acceleration
+
+    kalmanAng[0] = kalmanAng[0] + DELTA_T * kalmanInput                                   # prediction with gyro
+    kalmanAng[1] = kalmanAng[1] + DELTA_T * DELTA_T * STD_DEV_GYRO * STD_DEV_GYRO          # uncertainty of prediction
+    kalmanGain = kalmanAng[1] * 1 / (1 * kalmanAng[1] + STD_DEV_ACC * STD_DEV_ACC)         # kalman gain
+    kalmanAng[0] = kalmanAng[0] + kalmanGain * (kalmanMeasurement - kalmanAng[0])          # new prediction with tilt angle by acceleration associated with kalman gain
+    kalmanAng[1] = (1 - kalmanGain) * kalmanAng[1]                                        # new uncertainty prediction
+
+    return kalmanAng
+
+def getYaw():
+    # Function for calculating the Yaw.
+    # Warning: since this is not going through the Kalman Filter the readings are not reasonable to calculate the Yaw.
+
+    global gyroYaw
+    gyroYaw = gyroYaw + DELTA_T * omegaYaw
---
+++ senser.py
@@ -0,0 +1,72 @@
+import numpy as np
+import serial.tools.list_ports
+
+
+def select_port(silence=True):
+    # Get a list of all available serial ports
+    available_ports = list(serial.tools.list_ports.comports())
+
+    # If no serial ports are found, print an error message and exit the program
+    if len(available_ports) == 0:
+        print("No available serial ports found.")
+        exit()
+    if not silence:
+        # Print the available serial ports to the console
+        print("Available serial ports:")
+        for port in available_ports:
+            print(port.device)
+
+    # Choose the Arduino serial port
+    arduino_port = None
+    for port in available_ports:
+        if "Arduino" in port.description:
+            arduino_port = port.device
+            break
+
+    # If an Arduino serial port is not found, print an error message and exit the program
+    if arduino_port is None:
+        print("Arduino not found on any serial port.")
+        exit()
+
+    # Print the chosen Arduino serial port to the console
+    print("Arduino found on serial port:", arduino_port)
+
+    return arduino_port
+
+
+
+class Senser():
+    def __init__(self,_name,_serial, gyro_cali_data:list):
+
+        self.name = _name.encode('ascii')
+        self.raw_data = np.zeros([3,3])
+        self.calibrated_data = np.zeros([3,3])
+        self.angles = np.zeros([3])
+        self.serial = _serial
+        self.gyro_cali_data= np.array([[0,0,0],gyro_cali_data,[0,0,0]]) # set gyro calibrated data to (3,3) array it should be this shape [1,3,2]
+
+
+
+    def get_data(self):
+        """ this function send the name of the sensors to collect the data and read the bytes """
+        self.serial.write(self.name)
+        line = serial.readline()
+        self.convert_data(line)
+
+    def convert_data(self,line:str):
+
+        data =[float(i) for i in line.split(',')] # split the serial reading with the ','
+        self.raw_data = np.array(data).reshape(3, 3) # set the readings to raw data format
+
+    def gyroscope_calibration(self):
+        self.raw_data += self.gyro_cali_data  # add calibrated giro values to row data sine other are Zeros it would not affect
+
+
+
+
+
+
+
+
+
+
